1d2119e95e47bafdcfe7e7ae10b6527d092503a2
Seoksoon Jang <boraseoksoon@gmail.com>
2017-09-24 16:39:26 +0900

    [ADD] backend server routing
    + 2 space tap indentation

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -1,35 +1,40 @@
 import Vapor
 
 extension Droplet {
-    func setupRoutes() throws {
-
-        
-        get { req in
-            return try self.view.make("intro.html")
-        }
-        
-        get("resume") { req in
-            return try self.view.make("resume.html")
-        }
-        
-        get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
-        }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
-        get("description") { req in return req.description }
-        
-        try resource("posts", PostController.self)
+  func setupRoutes() throws {
+    
+    
+    get { req in
+      return try self.view.make("intro.html")
     }
+    
+    get("resume") { req in
+      return try self.view.make("resume.html")
+    }
+    
+    get("test") { req in
+      return try self.view.make("sampleHTMLCode.html")
+    }
+    
+    
+    get("hello") { req in
+      var json = JSON()
+      try json.set("hello", "world")
+      return json
+    }
+    
+    get("plaintext") { req in
+      return "Hello, world!"
+    }
+    
+    // response to requests to /info domain
+    // with a description of the request
+    get("info") { req in
+      return req.description
+    }
+    
+    get("description") { req in return req.description }
+    
+    try resource("posts", PostController.self)
+  }
 }
